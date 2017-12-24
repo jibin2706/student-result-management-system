33a5c76251ec8023a7cbad9e3bf8d5c630ed39e8
Jibin Thomas <jibin.thomas2706@gmail.com>
2017-12-24 21:04:16 +0530

    80% of the project completed

---
+++ add_results.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/home.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="./css/font-awesome-4.7.0/css/font-awesome.css">
+    <link rel="stylesheet" href="./css/form.css">
+    <title>Dashboard</title>
+</head>
+<body>
+        
+    <div class="title">
+        <img src="./images/logo1.png" alt="" class="logo">
+        <span class="heading">Dashboard</span>
+        <a href="logout.php" style="color: white"><span class="fa fa-sign-out fa-3x"></span></a>
+    </div>
+
+    <div class="nav">
+        <ul>
+            <li class="dropdown" onclick="toggleDisplay('1')">
+                <a href="" class="dropbtn">Classes &nbsp
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <div class="dropdown-content" id="1">
+                    <a href="add_classes.php">Add Class</a>
+                    <a href="manage_classes.php">Manage Class</a>
+                </div>
+            </li>
+            <li class="dropdown" onclick="toggleDisplay('2')">
+                <a href="#" class="dropbtn">Subjects &nbsp
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <div class="dropdown-content" id="2">
+                    <a href="add_subjects.php">Add Subjects</a>
+                    <a href="manage_subjects.php">Manage Subjects</a>
+                </div>
+            </li>
+            <li class="dropdown" onclick="toggleDisplay('3')">
+                <a href="#" class="dropbtn">Students &nbsp
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <div class="dropdown-content" id="3">
+                    <a href="add_students.php">Add Students</a>
+                    <a href="manage_students.php">Manage Students</a>
+                </div>
+            </li>
+            <li class="dropdown" onclick="toggleDisplay('4')">
+                <a href="#" class="dropbtn">Results &nbsp
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <div class="dropdown-content" id="4">
+                    <a href="add_results.php">Add Results</a>
+                    <a href="manage_results.php">Manage Results</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <div class="main">
+        <form action="" method="post">
+            <fieldset>
+            <legend>Enter Marks</legend>
+
+                <?php
+                    include("init.php");
+                    include("session.php");
+                    $db = mysqli_select_db($conn,'srms');
+
+                    $select_class_query="SELECT `name` from `class`";
+                    $class_result=mysqli_query($conn,$select_class_query);
+                    //select class
+                    echo '<select name="class_name">';
+                        while($row = mysqli_fetch_array($class_result)) {
+                            $display=$row['name'];
+                            echo '<option value="'.$display.'">'.$display.'</option>';
+                        }
+                    echo'</select>';
+                    
+
+                    // // select student
+                    // // $sql="SELECT `name` from `students` WHERE `name` not in (SELECT `name` from `result`) and (SELECT `name` from `students` WHERE `class_name`=)";
+                    // $sql="SELECT `name`,`rno` from `students` WHERE `name` in (SELECT `name` from `students` WHERE `class_name`='fybsc it') and name not in (SELECT `name` from `result`)";
+                    // $student_result=mysqli_query($conn,$sql);
+                    // echo '<select name="student_name">';
+                    // echo '<option selected disabled>Roll No</option>';
+                    // while($row = mysqli_fetch_array($student_result)) {
+                    //     $display=$row['name'];
+                    //     echo '<option value="'.$value.'">'.$display.'</option>';
+                    // }
+                    //     echo'</select>'
+                      
+                ?>
+                <input type="text" name="rno" placeholder="Roll No">
+                <input type="text" name="p1" id="" placeholder="Paper 1">
+                <input type="text" name="p2" id="" placeholder="Paper 2">
+                <input type="text" name="p3" id="" placeholder="Paper 3">
+                <input type="text" name="p4" id="" placeholder="Paper 4">
+                <input type="text" name="p5" id="" placeholder="Paper 5">
+                <input type="submit" value="Submit">
+            </fieldset>
+        </form>
+    </div>
+
+    <div class="footer">
+        <span>Designed & Coded By Jibin Thomas</span>
+    </div>
+</body>
+</html>
+
+<?php
+    if(isset($_POST['rno'],$_POST['p1'],$_POST['p2'],$_POST['p3'],$_POST['p4'],$_POST['p5'])) {
+        $rno=$_POST['rno'];
+        $class_name=$_POST['class_name'];
+        $p1=(int)$_POST['p1'];
+        $p2=(int)$_POST['p2'];
+        $p3=(int)$_POST['p3'];
+        $p4=(int)$_POST['p4'];
+        $p5=(int)$_POST['p5'];
+
+        $marks=$p1+$p2+$p3+$p4+$p5;
+        $percentage=$marks/5;
+        $name=mysqli_query($conn,"SELECT `name` FROM `students` WHERE `rn`='$rno' and `class_name`='$class_name'");
+        echo $name;
+        $sql="INSERT INTO `result` (`name`, `rno`, `class`, `p1`, `p2`, `p3`, `p4`, `p5`, `marks`, `percentage`) VALUES ('$name', '$rno', '$class_name', '$p1', '$p2', '$p3', '$p4', '$p5', '$marks', '$percentage')";
+        $result=mysqli_query($conn,$sql);
+        if (!$result) {
+            echo  mysqli_error($conn);
+            exit();
+        }
+        else{
+            echo"done";
+        }
+    }
+
+?>
---
+++ add_students.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/home.css">
+    <link rel="stylesheet" type="text/css" href="./css/form.css" media="all">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="./css/font-awesome-4.7.0/css/font-awesome.css">
+    <title>Add Students</title>
+</head>
+<body>
+        
+    <div class="title">
+        <img src="./images/logo1.png" alt="" class="logo">
+        <span class="heading">Dashboard</span>
+        <a href="logout.php" style="color: white"><span class="fa fa-sign-out fa-3x"></span></a>
+    </div>
+
+    <div class="nav">
+        <ul>
+            <li class="dropdown" onclick="toggleDisplay('1')">
+                <a href="" class="dropbtn">Classes &nbsp
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <div class="dropdown-content" id="1">
+                    <a href="add_classes.php">Add Class</a>
+                    <a href="manage_classes.php">Manage Class</a>
+                </div>
+            </li>
+            <li class="dropdown" onclick="toggleDisplay('2')">
+                <a href="#" class="dropbtn">Subjects &nbsp
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <div class="dropdown-content" id="2">
+                    <a href="add_subjects.php">Add Subjects</a>
+                    <a href="manage_subjects.php">Manage Subjects</a>
+                </div>
+            </li>
+            <li class="dropdown" onclick="toggleDisplay('3')">
+                <a href="#" class="dropbtn">Students &nbsp
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <div class="dropdown-content" id="3">
+                    <a href="add_students.php">Add Students</a>
+                    <a href="manage_students.php">Manage Students</a>
+                </div>
+            </li>
+            <li class="dropdown" onclick="toggleDisplay('4')">
+                <a href="#" class="dropbtn">Results &nbsp
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <div class="dropdown-content" id="4">
+                    <a href="add_results.php">Add Results</a>
+                    <a href="manage_results.php">Manage Results</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <div class="main">
+        <form action="" method="post">
+            <fieldset>
+                <legend>Add Student</legend>
+                Student Name: <input type="text" name="student_name" id="">
+                Roll No: <input type="text" name="roll_no" id="">
+                <!--  -->
+                <?php
+                    include('init.php');
+                    include('session.php');
+                    $db = mysqli_select_db($conn,'srms');
+                    $class_result=mysqli_query($conn,"SELECT `name` FROM `class`");
+                        echo 'Class: <select name="class_name">';
+                        echo '<option selected disabled>Select Class</option>';
+                    while($row = mysqli_fetch_array($class_result)){
+                        $display=$row['name'];
+                        echo '<option value="'.$display.'">'.$display.'</option>';
+                    }
+                    echo'</select>'
+                ?>
+                <!--  -->
+                <input type="submit" value="Submit">
+            </fieldset>
+        </form>
+    </div>
+
+    <div class="footer">
+        <span>Designed & Coded By Jibin Thomas</span>
+    </div>
+</body>
+</html>
+
+<?php
+    $db = mysqli_select_db($conn,'srms');
+
+    if(isset($_POST['student_name'],$_POST['roll_no'])) {
+        $name=$_POST['student_name'];
+        $rno=$_POST['roll_no'];
+        $class_name=$_POST['class_name'];
+        $sql = "INSERT INTO `students` (`name`, `rno`, `class_name`) VALUES ('$name', '$rno', '$class_name')";
+        $result=mysqli_query($conn,$sql);
+        if (!$result) {
+            echo "invalid student name or rn";
+            exit();
+        }
+        else{
+            echo "successful";
+        }
+
+    }
+?>
